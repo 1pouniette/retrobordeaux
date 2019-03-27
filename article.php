1b29f61ec37e928f5b8cbe0f51661bd223d23d2c
EliseDugue <elise.dugue@mmibordeaux.com>
2019-03-27 11:29:15 +0100

    recreate github repository

---
+++ article.php
@@ -0,0 +1,111 @@
+<!doctype html>
+
+<html lang="fr">
+
+<head>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Rétrobordeaux</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+
+</head>
+
+<body>
+
+		<header>
+		
+		<!-- LOGO -->
+		<img class='mx-auto d-block'src="img/logo.png" href="#" alt="Logo Rétrobordeaux" height="25%" width="25%">
+
+		<!-- Navbar : début -->
+  
+		<nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-center">
+		    <ul class="navbar-nav">
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Contact</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Accueil</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">A propos</a>
+		      </li>
+		    </ul>
+		</nav>
+
+		<!-- Navbar : fin -->
+
+	</header>
+
+		<div class='container col-6'>
+
+			<br><h1>Oubliez votre âge au salon du vintage !</h1><br>
+			
+			<h5>Dans le hangar 14, au milieu de la modernité du quai des marques, le Salon du Vintage a ouvert ses portes le 9 et 10 mars dernier. Ce ralliement des époques invite à (re)découvrir des objets du quotidien, de l’ameublement à l’automobile, en passant par la mode ou la musique. Pourquoi un tel engouement ? Eléments de réponse ci-dessous.</h5><br>
+			
+			<img src="img/salon-du-vintage.jpg" class="card-img" alt="Exposition au salon du vintage.">
+			
+			<br><br><p>Difficile de ne pas prendre “un coup de vieux” quand un enfant vous demande interloqué, à quoi ressemble une disquette ? Pas de panique, vous avez enfin l’occasion de retrouver les objets si chers à votre enfance, ou de découvrir celle de vos parents !<br> <br> 
+ 
+			Pour cette nouvelle édition, le salon vous accueille : collectionneurs, nostalgiques, ou simplement désireux de découvrir les tendances d'antan. Avec ses 3000m² de vêtements, vous trouverez votre bonheur. Tenues de grands couturiers comme Saint Laurent ou Dior, bons plans dans l'espace Frip Chic, mais aussi des meubles, objets de collection ou vinyles se partagent les allées avec la foule. Pas de moderne, tous ces objets datent des années 50 aux années 80.<br> <br> 
+
+			Cette année, le salon met en avant les objets iconiques. Le minitel, le Rubik’s cube, les consoles Atari, les chaises Panton ou encore le walkman ne vous rappellent-ils pas des souvenirs ? Ils sont entrés dans les moeurs grâce à leur design innovant, leur concept révolutionnaire ou simplement leur utilité. Le salon du vintage vous les a retrouvés pour une exposition hommage ! Vous avez donc le plaisir de revivre les années 70 avec les lignes space, les couleurs flashy des années 80 et le formalisme des années 90.<br> <br> 
+
+			Ce salon n'est pas uniquement un lieu de vente, vous y retrouvez de nombreux stands spéciaux. Les tatoueurs et "barbershop", les salons de coiffure, de maquillage vintage et les habilleurs sont prêts à vous téléporter dans le temps. Pendant ce temps, les enfants ont accès à un espace "rétrogaming" leur permettant de découvrir les jeux vidéo à l'époque de leur parents.
+			</p>
+			
+			<br><h4>Le rétro, pas seulement une tendance.</h4><br>
+			
+			<p>La tendance rétro est partout : que ce soit dans nos maisons ou même dans notre garde-robe, elle ne nous quitte plus. Issu des années 60, ce courant connaît un succès fulgurant depuis ces dernières années. D’où vient cette popularité ? La génération Y, située entre les années 1980 et 2000, est particulièrement touchée par cette tendance. <br> <br> 
+
+			En effet, notre monde moderne en plein accroissement berce les millenials. Par manque de repères, cette génération se rassure en se rattachant au passé. Autrefois abandonné pour des raisons d’amélioration vers un monde moderne, le vintage est aujourd’hui sollicité et idéalisé. Les initiés s’approprient le look du passé, pour le remettre au goût du jour.<br> <br> 
+
+			Le vintage permet une forme d’indépendance face à l’industrie de la mode. Avec les réseaux sociaux, l’uniformisation est un phénomène courant. Les vêtements de seconde-main encouragent donc l’individualité. Chaque individu devient son propre designer de tenues personnalisées. Cette génération trouve alors à travers cette tendance un moyen de se distinguer et de s’affirmer dans une société trop standardisée.
+			</p>
+			
+			<br><h4>“C’était mieux avant !”</h4><br>
+			
+			<p>Le vintage développe une certaine fascination pour les objets du passé, que cela soit en ameublement, automobile ou encore mode et beauté.  Reprendre les succès de chaque décennie et les transposer au goût du jour permet de se réapproprier une époque face à un futur incertain.<br><br>  
+
+			Qui n’a jamais économisé en achetant les meubles du voisin au vide-grenier, trouvé une pépite dans une brocante ou une pièce rare dans une friperie ? Le vintage est moins cher et se démocratise à travers les générations.<br> <br> 
+
+			L’avantage de consommer vintage est de pouvoir échanger, récupérer et réutiliser des objets. Cette vieille console que quelqu’un jetterait à la déchetterie ferait certainement le bonheur d’un collectionneur, attirait l’attention d’un enfant ou rendrait des parents nostalgiques. La réparation et la restauration appartiennent aussi à l’économie du vintage. Un coup de peinture sur le meuble de grand-père et il est comme neuf ! Une visite chez le garagiste et votre 2 CV repart comme sur des roulettes !<br> <br> 
+
+			En plus d’être économique, le vintage est écologique. En effet, la réparation et la réutilisation permet de donner une seconde vie à des objets. L’achat est plus durable et s’oppose aux problèmes de consommation moderne tels que l’obsolescence programmée.
+			</p>
+			
+			<h6>Le vintage suscite un véritable engouement au fur et à mesure des années et restera un phénomène intergénérationnel indémodable. Dans un monde ultra-normalisé, le vintage permet aux gens de se reconnecter avec le passé et de se démarquer en portant des pièces uniques. Alors n'oubliez pas de garder vos vieux vêtements ou les meubles qui vous semblent démodés, ils feront peut-être le bonheur de quelqu’un dans quelques années. Comme le dit Laurent Journo, créateur du salon du vintage, “Les créations d’aujourd’hui sont le vintage de demain”. </h6>
+			
+			<p>mots-clés : vintage, évènement, salon, exposition, collection, générations, seconde-main, recyclage, récupération </p>
+
+			<p><a href='https://salonduvintage.com/'>En savoir plus sur l’évenement</a><br><br>
+			<a href='https://www.researchgate.net/publication/263469162'>Etude sociétale concernant le vintage (anglais) - The Rise of Vintage Fashion and the Vintage Consumer</a> 
+			</p>
+
+			<p>Elise DUGUE, Julie HELDERLE, Coralie LEFEVRE, Tracy NG SING KWONG.</p>
+
+		</div>	
+
+	<footer>
+		
+		<!-- Lien CGU -->
+
+		<ul class="nav navbar-light bg-light justify-content-end">
+		  <li class="nav-item">
+		    <a class="nav-link" href="#">Conditions générales d'utilisation</a>
+		  </li>
+		</ul>
+
+		<!-- Fin lien CGU -->
+
+	</footer>
+
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,92 @@
+<!doctype html>
+
+<html lang="fr">
+
+<head>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Rétrobordeaux</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+
+</head>
+
+<body>
+
+	<header>
+		
+		<!-- LOGO -->
+		<img class='mx-auto d-block'src="img/logo.png" href="#" alt="Logo Rétrobordeaux" height="25%" width="25%">
+
+		<!-- Navbar : début -->
+  
+		<nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-center">
+		    <ul class="navbar-nav">
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Contact</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Accueil</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">A propos</a>
+		      </li>
+		    </ul>
+		</nav>
+
+		<!-- Navbar : fin -->
+
+	</header>
+
+	<!-- Marges pour l'aperçu des articles -->
+
+	<div class='container col-6'>
+
+		<!-- Aperçu article 1 -->
+
+		<div class="pt-5 pb-5" class="card">
+		  <img src="img/salon-du-vintage.jpg" class="card-img" alt="Exposition au salon du vintage.">
+			  <div class="card-body">
+			    <h5 class="card-title">Oubliez votre âge au salon du vintage !</h5>
+			    <p class="card-text">Dans le hangar 14, au milieu de la modernité du quai des marques, le Salon du Vintage a ouvert ses portes le 9 et 10 mars dernier. Ce ralliement des époques invite à (re)découvrir des objets du quotidien, de l’ameublement à l’automobile, en passant par la mode ou la musique. Pourquoi un tel engouement ? Eléments de réponse.</p>
+			    <a href="article.php" class="btn btn-outline-dark">Lire l'article</a>
+			  </div>
+		</div>
+
+		<!-- Aperçu article 2 -->
+
+		<div class="pt-5 pb-5" class="card">
+		  <img src="img/mobilite.jpg" class="card-img" alt="Voiture de collection dans un paysage de campagne.">
+			  <div class="card-body">
+			    <h5 class="card-title">Mobilité urbaine : regardez dans le rétro !</h5>
+			    <p class="card-text">Passionnés de véhicules rétros, amoureux de Bordeaux, venez la découvrir ou redécouvrir sous un angle inédit. Emportez le vintage dans la voiture pour une virée au centre-ville. Cheveux au vent, roulez à travers les monuments et voyagez hors du temps.</p>
+			    <a href="#" class="btn btn-outline-dark">Lire l'article</a>
+			  </div>
+		</div>
+
+	</div>
+
+	<!-- Fin de l'aperçu des articles -->
+
+
+	<footer>
+		
+		<!-- Lien CGU -->
+
+		<ul class="nav navbar-light bg-light justify-content-end">
+		  <li class="nav-item">
+		    <a class="nav-link" href="#">Conditions générales d'utilisation</a>
+		  </li>
+		</ul>
+
+		<!-- Fin lien CGU -->
+
+	</footer>	
+
+</body>
+
+</html>
