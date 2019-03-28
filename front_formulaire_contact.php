84703a8ba0cd3acdce3ee06aa216d1583034fa15
JulieHelderle <julie.helderle@mmibordeaux.com>
2019-03-28 12:13:26 +0100

    j'ai modif le formulaire contact

---
+++ front_formulaire_contact.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+
+ <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+
+ <title>Contact</title>
+
+</head>
+
+<body>
+
+		<div class="form-group"><img src ="contact_onglet.svg" class="rounded mx-auto d-block" alt="contact"style= "margin: 0 auto; margin-top: 3%;max-width: 200px; height: 100px; background color: white; border-radius:3px;">
+
+			<img src ="onglets_barre_contact.svg" class="rounded mx-auto d-block" alt="contact"style= "margin-top:-4%; margin-left:35%; max-width: 300px; height: 100px; background color: white; border-radius: 3px;">
+		</div>		
+
+ <form>
+
+   <div class="form-group" style="margin: 0 auto; max-width: 500px; height: 100px; background-color: white; border-radius: 3px;">
+       <label for="exampleFormControlInput1"></label>
+       <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Email">
+   </div>
+
+   <div class="form-group" style="margin: 0 auto; max-width: 500px; height: 100px; background-color: white; border-radius: 3px;">
+       <label for="exampleFormControlTextarea1"></label>
+       <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Votre message..." rows="10"></textarea>
+       <br>
+       <button type="button" class="btn btn-outline-secondary">Envoyer</button>
+   </div>
+
+ </form>
+
+</body>
+
+</html>
+
+
+
+
+		
+
+	
